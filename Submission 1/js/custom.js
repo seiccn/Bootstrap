3949984e0776fd93d5ffee7662de9ad1591c7a02
seiccn <ness.stephanie@gmail.com>
2014-10-09 21:35:31 +0200

    2nd commit

---
+++ Submission 1/js/custom.js
@@ -0,0 +1,39 @@
+/*!
+ * Customized Javascript file
+ */
+
+/* 1. NAVBAR:
+CLOSE THE NAVBAR */
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+/* On scrolling, collapse the NAVBAR*/
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+/* Scrolling with JS Easing Plugin */
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+/* Tooltips */
+
+    jQuery(function ($) {
+        $("a").tooltip();
+    });
+
+
+
+
